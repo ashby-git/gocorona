e3bab393c945bfa12c18b24285384f788a26e0dd
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-01 12:56:25 +0900

    💄change position of nav items on scroll

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ toggle }) => {
       <NavbarContainer scrollNav={scrollNav}>
         <MenuIcon onClick={toggle} />
         <NavbarLogo />
-        <NavbarItemContainer>
+        <NavbarItemContainer scrollNav={scrollNav}>
           <NavbarItem
             href="https://en.wikipedia.org/wiki/COVID-19"
             target="_blank"
--- src/components/navbar/navbar.styles.jsx
+++ src/components/navbar/navbar.styles.jsx
@@ -4,20 +4,19 @@ import menuIcon from "../../assests/MENU_ICON.png";
 
 export const NavbarContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
+  border-radius: 0 0 10px 10px;
   position: sticky;
   top: 0;
   z-index: 10;
+  background-color: ${({ scrollNav }) => (scrollNav ? "white" : "transparent")};
   height: ${({ scrollNav }) => (scrollNav ? "50px" : "null")};
   box-shadow: ${({ scrollNav }) =>
     scrollNav ? "0px 1px 20px #c8cae7" : "null"};
   padding: ${({ scrollNav }) =>
     scrollNav ? "0 43px 0 52px" : "20px 43px 0 52px"};
 
-  @media (max-width: 750px) {
+  @media (max-width: 950px) {
     padding: ${({ scrollNav }) =>
       scrollNav ? "0 40px 0 20px" : "20px 40px 0 20px"};
   }
@@ -26,7 +25,7 @@ export const NavbarContainer = styled.div`
 export const NavbarLogo = styled.img.attrs({
   src: `${goCoronaLogo}`,
 })`
-  @media (max-width: 750px) {
+  @media (max-width: 950px) {
     margin: auto;
   }
 `;
@@ -35,17 +34,16 @@ export const MenuIcon = styled.img.attrs({
   src: `${menuIcon}`,
 })`
   display: none;
-  @media (max-width: 750px) {
+  @media (max-width: 950px) {
     display: block;
     cursor: pointer;
   }
 `;
 
 export const NavbarItemContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+  margin: ${({ scrollNav }) => (scrollNav ? "0 20px 0 auto" : "0 13% 0 auto")};
 
-  @media (max-width: 750px) {
+  @media (max-width: 950px) {
     display: none;
   }
 `;
@@ -69,7 +67,7 @@ export const NavbarItem = styled.a`
 
 export const NavbarButton = styled.div`
   margin-bottom: 5px;
-  @media (max-width: 750px) {
+  @media (max-width: 950px) {
     display: none;
   }
 `;
