a9d3eb8e2b2cf53e07175308567e5b38f696e9f9
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-10 10:11:37 +0900

    🚧start positioning elements on landing-page

--- src/components/landing-page.component.jsx
+++ src/components/landing-page.component.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import {
+  DoctorImg,
+  MidPage,
+  TempoVidLink,
+  TitleTextButtonWrapper,
+} from "./landing-page.styles";
 import TitleTextButton from "./title-text-button.component";
 
 const LandingPage = () => {
   return (
     <>
-      <TitleTextButton
-        title={"Take care of your family's "}
-        titleBlue={"health."}
-        text="All in one destination for COVID-19 health queries. Consult 10,000+ health workers about your concerns."
-      />
-      <TitleTextButton
-        title="Stay safe with Go"
-        titleRed="Corona."
-        text="24x7 Support and user friendly mobile platform to bring healthcare to your fingertips. Signup and be a part of the new health culture."
-      />
-      <TitleTextButton
-        title="Talk to "
-        titleBlue="experts."
-        text="Book appointments or submit queries into thousands of forums concerning health issues and prevention against noval Corona Virus."
-      />
+      <MidPage>
+        <TitleTextButtonWrapper>
+          <TitleTextButton
+            title={"Take care of your family's "}
+            titleBlue={"health."}
+            text="All in one destination for COVID-19 health queries. Consult 10,000+ health workers about your concerns."
+          />
+        </TitleTextButtonWrapper>
+        <DoctorImg />
+      </MidPage>
+      <TempoVidLink />
     </>
   );
 };
--- src/components/landing-page.styles.jsx
+++ src/components/landing-page.styles.jsx
@@ -2,13 +2,47 @@ import styled from "styled-components";
 
 import doctorImg from "../assests/Landing-img-doctor.svg";
 
+export const MidPage = styled.div`
+  display: flex;
+
+  @media (max-width: 950px) {
+    flex-wrap: wrap-reverse;
+    justify-content: center;
+  }
+`;
+
+export const TitleTextButtonWrapper = styled.div`
+  margin: 240px 80px 0 70px;
+
+  @media (max-width: 950px) {
+    margin: 25px 0px 0 0;
+  }
+`;
+
 export const DoctorImg = styled.img.attrs({
   src: `${doctorImg}`,
 })`
   height: 394px;
   width: 547px;
+  margin-top: 153px;
   @media (max-width: 950px) {
     height: 228px;
     width: 316px;
+    margin-top: 92px;
+  }
+`;
+
+export const TempoVidLink = styled.div`
+  height: 44px;
+  width: 228px;
+  background-color: red;
+
+  @media (max-width: 950px) {
+    height: 40px;
+    width: 40px;
+    position: absolute;
+    top: 200px;
+    left: 47%;
+    cursor: pointer;
   }
 `;
