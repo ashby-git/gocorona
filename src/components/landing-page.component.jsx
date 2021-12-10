b414903f2b241ac803a337e07f764c9fdf615e7d
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-10 14:20:12 +0900

    📱have VideoLinkGroup change place on small screen

--- src/components/landing-page.component.jsx
+++ src/components/landing-page.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {
   DoctorImg,
+  ImageContainer,
   MidPage,
   TempoVidLink,
+  VideoLinkGroupNormal,
+  VideoLinkGroupSmall,
   TitleTextButtonWrapper,
 } from "./landing-page.styles";
 import TitleTextButton from "./title-text-button.component";
@@ -11,16 +14,23 @@ const LandingPage = () => {
   return (
     <>
       <MidPage>
+        <ImageContainer>
+          <VideoLinkGroupSmall>
+            <TempoVidLink />
+          </VideoLinkGroupSmall>
+          <DoctorImg />
+        </ImageContainer>
         <TitleTextButtonWrapper>
           <TitleTextButton
             title={"Take care of your family's "}
             titleBlue={"health."}
             text="All in one destination for COVID-19 health queries. Consult 10,000+ health workers about your concerns."
           />
+          <VideoLinkGroupNormal>
+            <TempoVidLink />
+          </VideoLinkGroupNormal>
         </TitleTextButtonWrapper>
-        <DoctorImg />
       </MidPage>
-      <TempoVidLink />
     </>
   );
 };
--- src/components/landing-page.styles.jsx
+++ src/components/landing-page.styles.jsx
@@ -4,18 +4,26 @@ import doctorImg from "../assests/Landing-img-doctor.svg";
 
 export const MidPage = styled.div`
   display: flex;
+  flex-direction: row-reverse;
+  height: 100%;
+
+  justify-content: center;
+  gap: 4%;
+  /* margin: 0 5px; */
 
   @media (max-width: 950px) {
-    flex-wrap: wrap-reverse;
+    /* flex-wrap: wrap-reverse; */
+    display: grid;
     justify-content: center;
+    gap: 0;
   }
 `;
 
 export const TitleTextButtonWrapper = styled.div`
-  margin: 240px 80px 0 70px;
+  margin: 240px 0 0 0;
 
   @media (max-width: 950px) {
-    margin: 25px 0px 0 0;
+    margin: 25px 0 0 0;
   }
 `;
 
@@ -24,11 +32,31 @@ export const DoctorImg = styled.img.attrs({
 })`
   height: 394px;
   width: 547px;
-  margin-top: 153px;
+  /* margin-top: 153px; */
   @media (max-width: 950px) {
     height: 228px;
     width: 316px;
+    /* margin-top: 92px; */
+  }
+`;
+
+export const ImageContainer = styled.div`
+  /* height: max-content; */
+  /* width: max-content; */
+  margin-top: 153px;
+
+  /* height: 394px; */
+  /* width: 547px; */
+
+  @media (max-width: 950px) {
     margin-top: 92px;
+
+    /* height: 228px; */
+    /* width: 316px; */
+
+    TempoVidLink {
+      display: none;
+    }
   }
 `;
 
@@ -36,13 +64,38 @@ export const TempoVidLink = styled.div`
   height: 44px;
   width: 228px;
   background-color: red;
+  /* margin-top: 60px; */
 
   @media (max-width: 950px) {
     height: 40px;
     width: 40px;
-    position: absolute;
+    /* position: absolute;
     top: 200px;
-    left: 47%;
-    cursor: pointer;
+    left: 47%; */
+
+    /* position: relative; */
+    /* top: 50%; */
+    /* left: 41%; */
+    /* margin-top: 92px; */
+    /* margin-top: 0; */
+  }
+`;
+
+export const VideoLinkGroupSmall = styled.div`
+  display: none;
+
+  @media (max-width: 950px) {
+    display: block;
+    position: relative;
+    top: 50%;
+    left: 41%;
+    margin-top: 0;
+  }
+`;
+
+export const VideoLinkGroupNormal = styled.div`
+  margin-top: 150px;
+  @media (max-width: 950px) {
+    display: none;
   }
 `;
