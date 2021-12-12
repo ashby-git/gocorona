06c9b7a27bcc9b66079aed98a9016509f4055148
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-12 15:50:01 +0900

    ♻️have video src as a prop

--- src/components/UI/video-link-group.component.jsx
+++ src/components/UI/video-link-group.component.jsx
@@ -23,7 +23,7 @@ const VideoLinkGroup = (props) => {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/Y_N1rTPhv04"
+            src={props.source}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
