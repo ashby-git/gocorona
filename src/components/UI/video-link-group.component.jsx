e01e2da0a267b0786e96630ae68cf2b3a0ee4bb2
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-13 20:28:08 +0900

    📱add media queries for video modal

--- src/components/UI/video-link-group.component.jsx
+++ src/components/UI/video-link-group.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  Iframe,
   LinkText,
   PlayIconBlue,
   PlayIconWhite,
@@ -20,15 +21,13 @@ const VideoLinkGroup = (props) => {
     <>
       {showModal && (
         <VideoModal onCloseModal={closeModal}>
-          <iframe
-            width="560"
-            height="315"
+          <Iframe
             src={props.source}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
-          ></iframe>
+          ></Iframe>
         </VideoModal>
       )}
 
