55d6b44b9b19b3a735365fb63cfc4e96fdc3cc62
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-17 11:39:24 +0900

    💄import mobile screen images

--- src/components/mobile-page/mobile-page.component.jsx
+++ src/components/mobile-page/mobile-page.component.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import {
+  MobileImage,
   MobileLeftSide,
   MobilePageContainer,
   MobileRightSide,
+  MobileShadow,
 } from "./mobile-page.styles";
 
 const MobilePage = () => {
   return (
-    <div>
+    <>
       <MobilePageContainer>
-        <MobileLeftSide></MobileLeftSide>
+        <MobileLeftSide>
+          <MobileImage />
+          <MobileShadow />
+        </MobileLeftSide>
         <MobileRightSide></MobileRightSide>
       </MobilePageContainer>
-    </div>
+    </>
   );
 };
 
--- src/components/mobile-page/mobile-page.styles.jsx
+++ src/components/mobile-page/mobile-page.styles.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+import { mobileImage } from "../../assests/mobile-screen.svg";
+import { mobileShadow } from "../../assests/mobile-screen-shadow.svg";
+
 export const MobilePageContainer = styled.div`
   display: flex;
   background-color: white;
@@ -14,6 +17,10 @@ export const MobileLeftSide = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+export const MobileImage = styled.img.attrs({ src: `${mobileImage}` })``;
+
+export const MobileShadow = styled.img.attrs({ src: `${mobileShadow}` })``;
 
 export const MobileRightSide = styled.div`
   display: flex;
