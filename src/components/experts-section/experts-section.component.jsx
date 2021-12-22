28c5f3dd79de7b98fdaccd72b535bbbd9dd86423
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-22 11:55:54 +0900

    🚧set up files

---
+++ src/components/experts-section/experts-section.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StatisticsBox from "../statistics-box/statistics-box.component";
+
+const ExpertsSection = () => {
+  return (
+    <>
+      <StatisticsBox />
+    </>
+  );
+};
+
+export default ExpertsSection;
