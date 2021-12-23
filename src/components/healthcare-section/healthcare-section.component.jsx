62b628fa257519cb533da1f92bd32e6416d4e9a9
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-23 14:23:17 +0900

    🚧set up files

---
+++ src/components/healthcare-section/healthcare-section.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IconTextCard from "../icon-text-card/icon-text-card.component";
+import { HealthcareSectionContainer } from "./healthcare-section.styles";
+
+const HealthcareSection = () => {
+  return (
+    <HealthcareSectionContainer>
+      <IconTextCard />
+    </HealthcareSectionContainer>
+  );
+};
+
+export default HealthcareSection;
---
+++ src/components/icon-text-card/icon-text-card.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IconTextCard = () => {
+  return <></>;
+};
+
+export default IconTextCard;
