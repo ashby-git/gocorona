fdab10be7242d3b683a4ed5c3f7278bff4584b21
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-12-24 18:18:36 +0900

    ✨make video autoplay

--- src/components/experts-section/experts-section.component.jsx
+++ src/components/experts-section/experts-section.component.jsx
@@ -84,7 +84,7 @@ const ExpertsSection = () => {
                 <CloseIcon />
               </CloseButton>
               <Iframe
-                src="https://www.youtube.com/embed/Y_N1rTPhv04"
+                src="https://www.youtube.com/embed/Y_N1rTPhv04?autoplay=1"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
